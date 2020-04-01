0290f1a2eb3d9c1cc55b458ea9266b7e62e73084
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2020-04-01 18:42:20 -0500

    Fix for a problem with the previous bug fix

--- backwords clock.py
+++ backwords clock.py
@@ -149,7 +149,7 @@ def run():
         screen.fill([255]*3)
         display.blit(background, (0,0))
         
-        h, m, s = intl(*' '.join(time.asctime().split('  ')).split(' ')[3].split(':'))
+        h, m, s = intl(*' '.join(asctime().split('  ')).split(' ')[3].split(':'))
         h %= 12
         h *= 30
         m *= 6
